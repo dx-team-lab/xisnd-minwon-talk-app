92bfe1a1f915bb835e2590193d76fff638d95642
엄지성 <eom19861@gmail.com>
2026-03-10 01:40:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7395999358-1ab24",
+  "appId": "1:130372839689:web:c0aa9cbcc376cdabf70bd8",
+  "apiKey": "AIzaSyDU0J_Mk1uzi33RIykGSke85e0ceRP5hBk",
+  "authDomain": "studio-7395999358-1ab24.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "130372839689"
+};
